43bd1b41528ae60a8349f4c943758228ad0de3e9
refyrizaldi <refyrizaldi2@gmail.com>
2024-05-11 16:15:52 +0700

    initial commit

---
+++ frontend/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
