9d7a60d69ddafcf306a04f5be4d721ce59b856a1
Reyv <refyrizaldi@apps.ipb.ac.id>
2024-05-18 13:32:45 +0700

    Added frontend 2.0 to repository

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,4 @@
+// FAQ
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
@@ -14,3 +15,19 @@ faqs.forEach((faq) => {
         faq.classList.toggle("active");
     });
 });
+
+// Mawar
+function change1() {
+    document.getElementById('pic').src='/frontend/images/wAster.jpg';
+}
+function change2() {
+    document.getElementById('pic').src='/frontend/images/bAster.jpg';
+}
+
+// Krisan
+function kchange1() {
+    document.getElementById('pic2').src='/frontend/images/wKrisan.jpg';
+}
+function kchange2() {
+    document.getElementById('pic2').src='/frontend/images/pKrisan.jpg';
+}
